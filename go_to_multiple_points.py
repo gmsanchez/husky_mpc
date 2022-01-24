a5589eb1ea2e5acc1f5f3ea2539391a3665afa6d
Guido Sanchez <sanchez.guido@gmail.com>
2022-01-24 10:48:42 -0300

    Add unrap to go_to_point.py and create a script that allows the vehicle to move to a list of waypoints.

--- go_to_multiple_points.py
+++ go_to_multiple_points.py
@@ -25,6 +25,7 @@ ODOM = None
 Delta = .02
 Nx = 3
 Nu = 2
+node_rate = int(1/Delta)
 
 
 def toEulerAngle(odom_msg):
@@ -65,25 +66,34 @@ Qn = np.array([[150.0, 0.0, 0.0], [0.0, 150.0, 0.0], [0.0, 0.0, 0.01]])
 R = np.array([[25.0, 0.0], [0.0, 25.0]])
 
 if len(sys.argv)==3:
-    x_ref = np.array([float(sys.argv[1]),float(sys.argv[2]),0])
+    x_ref = [np.array([float(sys.argv[1]),float(sys.argv[2]),0])]
 else:
-    x_ref = np.array([10,10,0])
+    x_ref = [np.array([10,0,0]),
+             np.array([10,10,0.5*np.pi]),
+             np.array([0,10,np.pi]),
+             np.array([0,0,1.5*np.pi]),
+             np.array([-10,0,np.pi]),
+             np.array([-10,10,0.5*np.pi]),
+             np.array([0,10,0]),
+             np.array([0,-2,1.5*np.pi])]
 
 # Define stage cost and terminal weight.
-def lfunc(x,u):
-    return mpc.mtimes((x-x_ref).T,Q,(x-x_ref)) + mpc.mtimes(u.T,R,u)
-l = mpc.getCasadiFunc(lfunc, [Nx,Nu], ["x","u"], funcname="l")
+def lfunc(x,u,x_sp):
+    return mpc.mtimes((x-x_sp).T,Q,(x-x_sp)) + mpc.mtimes(u.T,R,u)
+largs = ["x","u","x_sp"]
+l = mpc.getCasadiFunc(lfunc, [Nx,Nu,Nx], largs, funcname="l")
 
-def Pffunc(x):
-    return mpc.mtimes((x-x_ref).T,Qn,(x-x_ref))
-Pf = mpc.getCasadiFunc(Pffunc, [Nx], ["x"], funcname="Pf")
+def Pffunc(x,x_sp):
+    return mpc.mtimes((x-x_sp).T,Qn,(x-x_sp))
+Pfargs = ["x","x_sp"]
+Pf = mpc.getCasadiFunc(Pffunc, [Nx,Nx], Pfargs, funcname="Pf")
 
 # Make optimizers. Note that the linear and nonlinear solvers have some common
 # arguments, so we collect those below.
 x0 = np.array([0.0,0.0,0.0])
 Nt = 20
-u_ub = np.array([4., 2.0])
-u_lb = np.array([-4., -2.0])
+u_ub = np.array([4., 3.0])
+u_lb = np.array([-4., -3.0])
 
 commonargs = dict(
     verbosity=0,
@@ -92,7 +102,10 @@ commonargs = dict(
     Pf=Pf,
     lb={"u" : np.tile(u_lb,(Nt,1))},
     ub={"u" : np.tile(u_ub,(Nt,1))},
+    sp={"x" : np.tile(x_ref[0],(Nt+1,1))},
+    funcargs={"l" : largs, "Pf": Pfargs}
 )
+
 Nlin = {"t":Nt, "x":Nx, "u":Nu}
 Nnonlin = Nlin.copy()
 Nnonlin["c"] = 2 # Use collocation to discretize.
@@ -112,7 +125,7 @@ def odom_callback(odom_msg):
 def start():
     
     # Turn this into an official ROS node named approach
-    rospy.init_node('approach', anonymous=True)
+    rospy.init_node('go_to_multiple_points', anonymous=True)
 
     # Subscribe to the /scan topic.  From now on
     # scan_callback will be called every time a new scan message is
@@ -122,80 +135,66 @@ def start():
     rospy.Subscriber("/ground_truth/state", Odometry, odom_callback)
 
     # Create a publisher object for sending Twist messages to the
-    # turtlebot_node's velocity topic. 
+    # husky_velocity_node's velocity topic. 
     vel_pub = rospy.Publisher('husky_velocity_controller/cmd_vel', Twist, queue_size=10)
     # Create a twist object. 
     # twist.linear.x represents linear velocity in meters/s.
     # twist.angular.z represents rotational velocity in radians/s.
     twist = Twist()
 
-    prev_yaw = 0.0
-    curr_yaw = 0.0
-
     # Wait until the first scan is available.
     while ODOM is None and not rospy.is_shutdown():
         rospy.sleep(.1)
 
-    # Rate object used to make the main loop execute at 10hz.
-    rate = rospy.Rate(50)
+    # Rate object used to make the main loop execute at 50hz.
+    rate = rospy.Rate(node_rate)
     t = 0
-    d_min = 0.1
+    d_min = 2.0  # [m]
+    prev_yaw = 0.0
+    curr_yaw = 0.0
 
-    while not rospy.is_shutdown() and t<1500:
+    while not rospy.is_shutdown(): # and t<1500:
 
-        # Back up if the scan is bad, or if we are too close. 
-        # if math.isnan(SCAN.ranges[320]) or SCAN.ranges[320] < target:
-        #     twist.linear.x = -0.1  # forward velocity in meters/second
-        #     twist.angular.z = 0   # rotation in radians/second
-        # else:
-        #     twist.linear.x = 0.1 
-        #     twist.angular.z = 0  
-
-        # print "Range: {:.3f}".format(SCAN.ranges[320])
-
-####
         _xk = np.zeros((Nx,))
         _xk[0] = ODOM.pose.pose.position.x
         _xk[1] = ODOM.pose.pose.position.y
-
         (_, _, curr_yaw) = toEulerAngle(ODOM)
+        curr_yaw = magical_unwrap(curr_yaw, prev_yaw)
+        prev_yaw = curr_yaw
+        _xk[2] = curr_yaw
+        
         # roll = euler[0]
         # pitch = euler[1]
         # curr_yaw = euler[2]
 
-        curr_yaw = magical_unwrap(curr_yaw, prev_yaw)
-        prev_yaw = curr_yaw
-
-        _xk[2] = curr_yaw
-
-
-        # We can stop early if we are already close to the origin.
-        if np.linalg.norm(_xk[:2]-x_ref[:2]) <= d_min:
-            print("At origin.")
-            break
+        # We can stop early if we are already close to the current setpoint.
+        if np.linalg.norm(_xk[:2]-x_ref[0][:2]) <= d_min:
+            if len(x_ref)>1:
+                x_ref.pop(0)
+                print("Setpoint reached.")
+            else:
+                print("Last setpoint reached. Terminating.")
+                # np.save("nav_list",NAV_LIST)
+                # np.save("ground_truth",GROUND_TRUTH)
+                break
 
         print ("xk",_xk)
-
+        if len(x_ref)>0:
+            solver.par["x_sp"] = x_ref[0]
+            print("x_sp: ",x_ref[0])
         solver.fixvar("x",0,_xk)
         solver.solve()
 
         # Print status and make sure solver didn't fail.
         print ("%d: %s" % (t,solver.stats["status"]))
-        # print "%s" % (solver.stats["status"])
         if solver.stats["status"] != "Solve_Succeeded":
             break
         else:
             solver.saveguess()
         _uk = np.squeeze(solver.var["u",0])
-####
+
         twist.linear.x = _uk[0]
         twist.angular.z = _uk[1]
-#        if ODOM.pose.pose.position.x<10:
-#            twist.linear.x = 0.5
-#        else:
-#            twist.linear.x = 0.0
-#            
-#        print ODOM.pose.pose.position.x
 
         vel_pub.publish(twist) # These velocities will be applied for .6 seconds
                                # unless another command is sent before that. 
